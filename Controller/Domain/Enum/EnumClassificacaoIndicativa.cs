183f68d51ab0b41d607f1622d625930bd4828376
Yuri Duque <yurithielmann83@gmail.com>
2020-08-11 20:04:03 -0300

    criando enum classificacao

---
+++ Controller/Domain/Enum/EnumClassificacaoIndicativa.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace Domain.Enum
+{
+    public enum EnumClassificacaoIndicativa
+    {
+        [Description("L")]
+        Livre = 1,
+
+        [Description("10")]
+        Dez = 10,
+
+        [Description("12")]
+        Doze = 12,
+
+        [Description("14")]
+        Quatorze = 14,
+
+        [Description("16")]
+        Dezesseis = 16,
+
+        [Description("18")]
+        Dezoito = 18
+    }
+}
