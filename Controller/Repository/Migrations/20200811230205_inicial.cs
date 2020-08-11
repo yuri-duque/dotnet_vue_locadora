17c3ec935fbc8b5da529da0003f057c9e1ea48b3
Yuri Duque <yurithielmann83@gmail.com>
2020-08-11 20:05:07 -0300

    adicionando inicial migration

---
+++ Controller/Repository/Migrations/20200811230205_inicial.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CPF = table.Column<string>(maxLength: 11, nullable: false),
+                    Nome = table.Column<string>(maxLength: 200, nullable: false),
+                    DataNascimento = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clientes", x => new { x.Id, x.CPF });
+                    table.UniqueConstraint("AK_clientes_Id", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "filmes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Titulo = table.Column<string>(maxLength: 100, nullable: false),
+                    ClassificacaoIndicativa = table.Column<int>(nullable: false),
+                    Lancamento = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_filmes", x => new { x.Id, x.Titulo });
+                    table.UniqueConstraint("AK_filmes_Id", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "locacoes",
+                columns: table => new
+                {
+                    IdCliente = table.Column<int>(nullable: false),
+                    IdFilme = table.Column<int>(nullable: false),
+                    DataLocacao = table.Column<DateTime>(nullable: false),
+                    DataDevolucao = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_locacoes", x => new { x.IdCliente, x.IdFilme });
+                    table.ForeignKey(
+                        name: "FK_locacoes_clientes_IdCliente",
+                        column: x => x.IdCliente,
+                        principalTable: "clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_locacoes_filmes_IdFilme",
+                        column: x => x.IdFilme,
+                        principalTable: "filmes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_locacoes_IdFilme",
+                table: "locacoes",
+                column: "IdFilme");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "locacoes");
+
+            migrationBuilder.DropTable(
+                name: "clientes");
+
+            migrationBuilder.DropTable(
+                name: "filmes");
+        }
+    }
+}
