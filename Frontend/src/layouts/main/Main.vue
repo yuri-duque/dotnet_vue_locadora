27195cec1cf5f545e0f993d3ae2d7e357fdb5a2d
Yuri Duque <yurithielmann83@gmail.com>
2020-08-13 02:33:40 -0300

    alterando logo do projeto

--- Frontend/src/layouts/main/Main.vue
+++ Frontend/src/layouts/main/Main.vue
@@ -13,7 +13,7 @@
 
     <v-nav-menu
       :navMenuItems = "navMenuItems"
-      title         = "Vuexy"
+      title         = "Locadoura"
       parent        = ".layout--main" />
 
     <div id="content-area" :class="[contentAreaClass, {'show-overlay': bodyOverlay}]">
